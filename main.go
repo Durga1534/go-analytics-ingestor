bfe719f165713dea45079e1c86b1d4a66a82211c
Durga1534 <durgaprasadpc143@gmail.com>
2026-04-17 06:54:42 +0530

    feat: implement redis queue and background worker

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/redis/go-redis/v9"
 )
 
 type Event struct {
@@ -14,13 +20,41 @@ type Event struct {
 	Timestamp time.Time `json:"timestamp"`
 }
 
+var (
+	rdb *redis.Client
+	ctx = context.Background() // Global Context for simple use cases
+)
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, proceeding with system evv variables")
+	}
+
+	redisURL := os.Getenv("REDIS_URL")
+	if redisURL == "" {
+		log.Fatal("REDIS_URL is not set in .env file")
+	}
+	//1 Safety: Intialize Redis and chexk connection
+	opt, err := redis.ParseURL("")
+	if err != nil {
+		log.Fatalf("Failed to parse Redis URL: %v", err)
+	}
+	rdb = redis.NewClient(opt)
+
+	// Verify connection (Safety First)
+	if _, err := rdb.Ping(ctx).Result(); err != nil {
+		log.Fatalf("Could not connect to Upstash: %v", err)
+	}
+	fmt.Println("Successfully connected to Upstash Redis!")
+
+	//2 Start the consumer (The Worker)
+	go startWorker()
+
 	http.HandleFunc("/ingest", ingestHandler)
 
 	fmt.Println("Server starting on :8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Printf("Server failed: %s\n", err)
-	}
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func ingestHandler(w http.ResponseWriter, r *http.Request) {
@@ -38,6 +72,13 @@ func ingestHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid JSON payload", http.StatusBadRequest)
 		return
 	}
+	payload, _ := json.Marshal(e)
+	err := rdb.LPush(ctx, "analytics_queue", payload).Err()
+	if err != nil {
+		log.Printf("Redis Push Error: %v", err)
+		http.Error(w, "Failed to queue event", http.StatusInternalServerError)
+		return
+	}
 	defer r.Body.Close()
 
 	//3. Send 202 Accepted immediately
@@ -52,6 +93,25 @@ func ingestHandler(w http.ResponseWriter, r *http.Request) {
 	}(e)
 }
 
+func startWorker() {
+	fmt.Println("Worker active: Listeninng for events...")
+	for {
+		result, err := rdb.BLPop(ctx, 0, "analytics_queue").Result()
+		if err != nil {
+			log.Printf("Worker Error: %v", err)
+			time.Sleep(2 * time.Second)
+			continue
+		}
+		var e Event
+		if err := json.Unmarshal([]byte(result[1]), &e); err != nil {
+			log.Printf("Failed to unmarshal event: %v", err)
+			continue
+		}
+		processEvent(e)
+
+	}
+}
+
 func processEvent(e Event) {
-	fmt.Printf("[%s] Logged: %s (ID: %s)\n", e.Timestamp.Format(time.RFC3339), e.Type, e.ID)
+	fmt.Printf("Worker Processed: %s | Type: %s\n", e.ID, e.Type)
 }
